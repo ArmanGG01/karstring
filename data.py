2f0dee6b0f901495a9fe666459fb65f73569e55e
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-10-15 02:15:56 +0700

    Update data.py

--- data.py
+++ data.py
@@ -2,11 +2,11 @@ from pyrogram.types import InlineKeyboardButton
 
 
 class Data:
-    generate_single_button = [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")]
+    generate_single_button = [InlineKeyboardButton("👑 Mulai Membuat string 👑", callback_data="generate")]
 
     home_buttons = [
         generate_single_button,
-        [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")]
+        [InlineKeyboardButton(text="🗿 Kembali Ke Awal 🗿", callback_data="home")]
     ]
 
     generate_button = [generate_single_button]
