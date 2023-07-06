34bb3347aa321d3a3ec1ffaffdbb7225bda35a02
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-07-06 13:45:15 +0700

    Update must_join.py

--- StringSessionBot/must_join.py
+++ StringSessionBot/must_join.py
@@ -2,7 +2,6 @@ from env import MUST_JOIN
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from Config import MUST_JOIN
 
 
 @Client.on_message(filters.incoming & filters.private, group=-1)
