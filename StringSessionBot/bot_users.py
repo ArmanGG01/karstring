e74e249eabbce21f848abd839a83b4ea98671eff
Sourcery AI <>
2023-08-24 05:54:06 +0000

    'Refactored by Sourcery'

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -7,12 +7,11 @@ from StringSessionBot.database.users_sql import Users, num_users
 @Client.on_message(~filters.service, group=1)
 async def users_sql(_, msg: Message):
     if msg.from_user:
-        q = SESSION.query(Users).get(int(msg.from_user.id))
-        if not q:
+        if q := SESSION.query(Users).get(int(msg.from_user.id)):
+            SESSION.close()
+        else:
             SESSION.add(Users(msg.from_user.id))
             SESSION.commit()
-        else:
-            SESSION.close()
 
 
 @Client.on_message(filters.user(1946995626) & filters.command("stats"))
